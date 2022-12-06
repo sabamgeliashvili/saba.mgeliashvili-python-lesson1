37b03146f1422825e794f18c2bde882218f7e7fd
sabamgeliashvili <sba69717@gmail.com>
2022-12-06 21:19:12 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,7 @@
+name = 'saba'
+age = 20
+height = 182.5
+location = 'tbilisi'
+
+print( 'chemi saxelia' + " " + name + " " + 'var' + " " + str(age) + " " + 'wlis' + " " + 'var' + " " + str(height) + " " 'chemi lokaciaa' + " " + location)
+
